37d3cdd78eff90486476e5e7cde8a119b4b6119e
Shyaar Sati Fwatshak <shyaarsati@gmial.com>
2025-03-01 01:42:10 +0100

    first commit

---
+++ grades.js
@@ -0,0 +1,24 @@
+let score;
+score = 90;
+
+if(score<45){
+    console.log("F")
+}
+else if(score>44 && score<50){
+    console.log("D")
+}
+else if(score>49 && score<70){
+    console.log("C")
+}
+else if(score>69 && score <90){
+    console.log("B")
+}
+else if(score >89){
+    console.log("A")
+}
+
+// 0 to 44 is failed
+// 45 to 49 is D
+// 50 to 69 is c 
+// 70- 89 is b 
+// 90 - 100 is a
